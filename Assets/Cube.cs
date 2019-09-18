448d00b330127de82460148d0f5d6118d3a2dfea
phawel <przemyslaw.hawel@alwasaet.com>
2019-09-18 09:17:49 +0200

    Initial commit

---
+++ Assets/Cube.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public bool HasExpired => !_isAlive;
+
+    private float _timeAlive;
+    private  float _lifetimeSeconds = 3.0f;
+    private bool _isAlive = true;
+
+    private Renderer _renderer;
+    private Material _mat;
+
+
+    void Awake()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        _mat = _renderer.material;
+    }
+
+    void Update()
+    {
+        CastRays();
+        _timeAlive += Time.deltaTime;
+
+        if (_timeAlive >= _lifetimeSeconds)
+        {
+            _isAlive = false;
+        }
+    }
+
+    private void CastRays()
+    {
+        int hits = 0;
+        if(Physics.Raycast(transform.position, Vector3.forward)) hits++;
+        if(Physics.Raycast(transform.position, Vector3.back)) hits++;
+        if(Physics.Raycast(transform.position, Vector3.left)) hits++;
+        if(Physics.Raycast(transform.position, Vector3.right)) hits++;
+
+        switch (hits)
+        {
+            case 0:
+                _mat.color = Color.black;
+                break;
+            case 1:
+                _mat.color = Color.blue;
+                break;
+            case 2:
+                _mat.color = Color.green;
+                break;
+            case 3:
+                _mat.color = Color.cyan;
+                break;
+            case 4:
+                _mat.color = Color.yellow;
+                break;
+        }
+    }   
+    
+    public void SetLifetime(float time)
+    {
+        _lifetimeSeconds = time;
+    }
+}
---
+++ Assets/CubesManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubesManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private Transform _spawnLocation;
+    [SerializeField] private GameObject[] _cubesOnScene;
+    
+    private const float _SPAWN_VARIANCE = 25;
+    private const float _LIFETIME_MIN = 8.0f;
+    private const float _LIFETIME_MAX = 12.0f;
+    private const int _MAX_CUBE_COUNT = 2500;
+
+    public int count;
+
+    private void Awake()
+    {
+        _cubesOnScene = new GameObject[_MAX_CUBE_COUNT];
+        Init();
+    }
+
+    private void Init()
+    {
+        for (var i = 0; i < _cubesOnScene.Length; i++)
+        {
+            var newCube = SpawnCube();
+            _cubesOnScene[i] = newCube;
+            count++;
+        }
+    }
+    
+    private void Update()
+    {
+        var i = 0;
+        for (i = 0; i < _cubesOnScene.Length; i++)
+        {
+            if (_cubesOnScene[i] == null)
+            {
+                var newCube = SpawnCube();
+                _cubesOnScene[i] = newCube;
+                count++;
+                return;
+            }
+            
+            var cube = _cubesOnScene[i].GetComponent<Cube>();
+            if (cube.HasExpired)
+            {
+                Destroy(_cubesOnScene[i]);
+                count--;
+            }
+        }
+        
+    }
+
+    private GameObject SpawnCube()
+    {
+        var x = Random.Range(-_SPAWN_VARIANCE, _SPAWN_VARIANCE);
+        var z = Random.Range(-_SPAWN_VARIANCE, _SPAWN_VARIANCE);
+        var location = _spawnLocation.position + new Vector3(x, 0, z);
+        var go = Instantiate(_cubePrefab, location, Quaternion.identity);
+        go.AddComponent<Rigidbody>();
+        var cubeController = go.AddComponent<Cube>();
+
+        var t = Random.Range(_LIFETIME_MIN, _LIFETIME_MAX);
+        cubeController.SetLifetime(t);
+
+        return go;
+    }
+}
