2b9a52fbaea1b675ba0df65dbc526d652cd69782
phawel <przemyslaw.hawel@alwasaet.com>
2019-09-19 08:32:09 +0200

    Task 1 created.

---
+++ Assets/Scripts/Task1/FollowTransform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/* This script causes the overhead camera to follow a specified Transform instance.
+ * The requirements are as follows:
+ * A. The camera moves on the X/Z plane.
+ * B. The camera has an acceleration and damping effect, meaning it's not "glued" to the
+ *    transform, but rather gently adjusts upon transform position change.
+ *
+ * C. Damping parameters can be adjusted via inspector.
+ */
+
+public class FollowTransform : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/Task1/ManController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/*
+ * This is a simple character controller. Requirements are as follows:
+ * A. The character moves at a constant speed according to WSAD keys input.
+ *     Movement direction is independent of character heading/rotation.
+ * B. Pressing the left Shift key increases movement speed by a percentage.
+ * C. Walking and running speed can be adjusted via the inspector.
+ * D. The character always rotates directly towards the mouse pointer on his Y-axis.
+ * 
+ *
+ * Please fill out the TurnTowardsMouse, ReadMovementInput, and Move methods.
+ * No model animation is required.
+ */
+
+public class ManController : MonoBehaviour
+{
+    private void Update()
+    {
+        var dir = ReadMovementInput();
+        Move(dir);
+        TurnTowardsMouse();
+    }
+
+    private void TurnTowardsMouse()
+    {
+        
+    }
+
+    private Vector3 ReadMovementInput()
+    {
+        return Vector3.zero;
+    }
+
+    private void Move(Vector3 direction)
+    {
+        
+    }
+}
